6b738c18ab4cb53907f886ef40430cf1f156380b
ed-nesma02 <shkurko26081978@gmail.com>
2023-07-17 07:17:04 +0300

    Day 9 fix 1

--- src/Components/FavoritePage/FavoritePage.jsx
+++ src/Components/FavoritePage/FavoritePage.jsx
@@ -5,16 +5,25 @@ import { fetchCategory } from "../../features/goodsSlice";
 import { usePageFromSearchParams } from "../../hooks/usePageFromSearchParams";
 import s from './FavoritePage.module.scss'
 import { Container } from "../Layout/Container/Container";
+import { useNavigate } from "react-router-dom";
 
 export const FavoritePage = () =>{
     const dispath = useDispatch();
     const favorites = useSelector(state => state.favorites);
+    const {pages} = useSelector(state=> state.goods)
     const page = usePageFromSearchParams(dispath);
+    const navigate = useNavigate();
+
+    console.log(pages);
 
     useEffect(()=>{
+        console.log(favorites.length%12)
         if(favorites){
             const param = {list : favorites};
-            if(page){
+            const favoritesLength = favorites.length;
+            if(!(favoritesLength%12) && page===Math.ceil((favoritesLength+1)/12)){
+                navigate(`/favorite?page=${page-1}`);
+            }else if(page){
                 param.page = page;
             }
             dispath(fetchCategory(param))
