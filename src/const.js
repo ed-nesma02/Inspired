fc6493827f20837bc3f74e2e6d862aa3994e7d49
ed-nesma02 <shkurko26081978@gmail.com>
2023-07-23 16:54:03 +0300

    API chose

--- src/const.js
+++ src/const.js
@@ -1,4 +1,4 @@
-export const API_URL = 'https://crawling-heavy-coal.glitch.me';
+export const API_URL = 'https://inspired-api.glitch.me';
 export const GOODS_URL = `${API_URL}/api/goods`;
 export const CATEGORIES_URL = `${API_URL}/api/categories`;
 export const COLORS_URL = `${API_URL}/api/colors`;
